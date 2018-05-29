26a9e26ea2b682d2bb089e4ea79ef51815c1e36f
Vincent Cote <vincent.cote@uleth.ca>
2018-05-29 02:35:38 +0000

    added some comments to uiButtons.py file

--- initializeScript/uiButtons.py
+++ initializeScript/uiButtons.py
@@ -1,3 +1,7 @@
+#Custom magic functions, to use call 'import uiButtons' in a cell
+#once the library has been imported magic commands may be used
+#eg. %uiButtons
+
 from __future__ import print_function
 from IPython.core.magic import Magics, magics_class, line_magic
 from IPython import get_ipython
@@ -5,6 +9,7 @@ from IPython import get_ipython
 @magics_class
 class MyMagics(Magics):
 
+    #When run it initilizes the 'show/hide code' buttons and the 'initialize' button
     @line_magic
     def uiButtons(self, line):
         raw_code = '''%%html
@@ -35,6 +40,8 @@ class MyMagics(Magics):
                 <input id="init" type="submit" value="Initialize">'''
 
         self.shell.run_cell(raw_code, store_history=False)
+        
+#define more custom magic function here as needed.
 
 ip = get_ipython()
 ip.register_magics(MyMagics)
