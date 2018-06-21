46a9cda860b95c2eebac93b2ef9c49cc64cd757a
Eric Easthope <eric.easthope@me.com>
2018-06-20 18:52:22 -0700

    Add a flag for immediate code wrapping and execution

--- nbvis/magics.py
+++ nbvis/magics.py
@@ -2,53 +2,79 @@ from IPython import get_ipython
 from IPython.core import magic_arguments
 from IPython.core.magic import Magics, magics_class, cell_magic
 
-"""Magics to inject JavaScript"""
+from .classes import Vis, D3
+
 @magics_class
-class vizMagics(Magics):
-    @cell_magic
-    @magic_arguments.magic_arguments()
-    @magic_arguments.argument('--reset', '-r',
-          action='store_true',
-          help='Replace any elements of mathbox_code with the current cell body'
-    )
-    def mathbox(self, line, cell):
-        args = magic_arguments.parse_argstring(self.mathbox, line)
-        if args.reset:
-            ip.user_ns['mathbox_code'] = []
-        try:
-            mathBoxCode = ip.user_ns['mathbox_code']
-            if cell in mathBoxCode:
-                print("Code duplicate! Not added to MathBox visualization stack.")
-            else:
-                ip.user_ns['mathbox_code'].append(cell)
-                print("Code added to MathBox visualization stack!")
-        except KeyError:
-            ip.user_ns['mathbox_code'] = []
-            ip.user_ns['mathbox_code'].append(cell)
-            print("Created MathBox container.")
-            print("Code added to MathBox visualization stack!");
+class visMagics(Magics):
+    """
+    Magics to store JavaScript visualization code
+    """
     
     @cell_magic
     @magic_arguments.magic_arguments()
-    @magic_arguments.argument('--reset', '-r',
+    @magic_arguments.argument('--reset',
           action='store_true',
-          help='Replace any elements of d3_code with the current cell body'
+          help='Reset d3_code to an empty list before code added from current cell body'
+    )
+    @magic_arguments.argument('--now',
+          action='store_true',
+          help='Run code from current cell body immediately'
     )
     def d3(self, line, cell):
+        """
+        """
+        
         args = magic_arguments.parse_argstring(self.d3, line)
         if args.reset:
             ip.user_ns['d3_code'] = []
-        try:
-            if cell in ip.user_ns['d3_code']:
-                print("Code duplicate! Not added to D3 visualization stack.")
-            else:
+            print('Initialized d3_code container!')
+        if args.now:
+            null = Vis(js=cell)
+        else:
+            try:
+                if cell in ip.user_ns['d3_code']:
+                    print('Duplicate code not added to D3 visualization stack ...')
+                else:
+                    ip.user_ns['d3_code'].append(cell)
+                    print('Code added to D3 visualization stack ...')
+            except KeyError:
+                ip.user_ns['d3_code'] = []
                 ip.user_ns['d3_code'].append(cell)
-                print("Code added to D3 visualization stack!")
-        except KeyError:
-            ip.user_ns['d3_code'] = []
-            ip.user_ns['d3_code'].append(cell)
-            print("Created D3 container.")
-            print("Code added to D3 visualization stack!");
+                print('Initialized d3_code container!')
+                print('Code added to D3 visualization stack ...');
+                
+    @cell_magic
+    @magic_arguments.magic_arguments()
+    @magic_arguments.argument('--reset',
+          action='store_true',
+          help='Reset mathbox_code to an empty list before code added from current cell body'
+    )
+    @magic_arguments.argument('--now',
+          action='store_true',
+          help='Run code from current cell body immediately'
+    )
+    def mathbox(self, line, cell):
+        """
+        """
+        
+        args = magic_arguments.parse_argstring(self.mathbox, line)
+        if args.reset:
+            ip.user_ns['mathbox_code'] = []
+            print('Initialized mathbox_code container!')
+        if args.now:
+            null = Vis(js=cell)
+        else:
+            try:
+                if cell in ip.user_ns['mathbox_code']:
+                    print('Duplicate code not added to MathBox visualization stack ...')
+                else:
+                    ip.user_ns['mathbox_code'].append(cell)
+                    print('Code added to MathBox visualization stack ...')
+            except KeyError:
+                ip.user_ns['mathbox_code'] = []
+                ip.user_ns['mathbox_code'].append(cell)
+                print('Initialized mathbox_code container!')
+                print('Code added to MathBox visualization stack ...');
 
 ip = get_ipython()
-ip.register_magics(vizMagics)
+ip.register_magics(visMagics)
