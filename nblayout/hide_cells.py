bd76d1d7d018eb90195eb80a3d1c090a24c5f710
Eric Easthope <eric.easthope@me.com>
2018-07-05 19:35:52 -0700

    Add loader for cell hiding toolbar and toggles

---
+++ nblayout/hide_cells.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# hide_cells.py
+
+'''
+Append toolbar controls and toggles to set the
+visibility of cells in Jupyter notebooks
+
+Adds a new cell metadata attribute to hidden cells
+
+Assembled by Eric Easthope
+'''
+
+import os
+from nbvis.classes import Vis
+from IPython.display import HTML, Javascript
+
+module_directory = os.path.dirname(os.path.abspath(__file__))
+
+style_filepath = os.path.join(module_directory, 'toggle_style.css')
+toggle_filepath = os.path.join(module_directory, 'add_toggles.js')
+toolbar_filepath = os.path.join(module_directory, 'append_toolbar.js')
+
+with open(style_filepath, 'r') as f:
+    display(HTML(f.read()))
+
+with open(toolbar_filepath, 'r') as toolbar:
+    with open(toggle_filepath, 'r') as toggles:
+        Vis(js=toolbar.read() + '\n' + toggles.read());
