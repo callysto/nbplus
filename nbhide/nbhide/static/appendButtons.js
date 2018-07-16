c1b626d0bcf21d22f0c83f1312c2f7cdb96bc5f6
Eric Easthope <eric.easthope@me.com>
2018-07-16 16:37:52 -0700

    Add button to Run All

--- nbhide/nbhide/static/appendButtons.js
+++ nbhide/nbhide/static/appendButtons.js
@@ -7,7 +7,7 @@ requirejs.config({
 requirejs(["d3"], d3 => {
     var container = d3.select("div.container-fluid");
     var kernelIndicator = d3.select("#kernel_indicator");
-    
+
     var width = kernelIndicator
                     .node()
                         .getBoundingClientRect()
@@ -17,14 +17,14 @@ requirejs(["d3"], d3 => {
                     .node()
                         .getBoundingClientRect()
                         .height;
-    
+
     // configure default display behaviour (boolean)
     var codeActive = false,
         markdownActive = true;
-    
+
     var codeCells = d3.selectAll('div.code_cell'),
         markdownCells = d3.selectAll('div.text_cell');
-    
+
     // (default) initialize nonempty notebook with all code cells hidden
     if (Jupyter.notebook.get_cells().length > 1) {
         codeCells.each(function () {
@@ -40,28 +40,28 @@ requirejs(["d3"], d3 => {
                               .classed("navbar-text", true)
                                   .style("float", "right")
                                   .style("margin-left", "5px");
-    
+
     d3.svg("https://ionicons.com/ionicons/svg/md-code.svg").then(function(svg) {
-        var button = buttonContainer
-                             .append("g")
-                                 .attr("transform", "translate(" + [-width/4, 0] + ")")
-                                 .style("fill", "#777")
-        
+        var button = buttonContainer.append("g")
+
+        button.style("fill", "#777");
         button.style("opacity", codeActive ? 1 : 1/4);
         button.html(svg.querySelector("svg").outerHTML);
-        
+
         var buttonWidth = button.node().getBoundingClientRect().width,
             buttonHeight = button.node().getBoundingClientRect().height;
-        
+
+        button.attr("transform", "translate(" + [-width/3, 0] + ")");
+
         var rect = button.insert("rect")
                            .attr("x", width/2 - buttonWidth/2)
                            .attr("y", height/2 - buttonHeight/2)
                            .attr("width", buttonWidth)
                            .attr("height", buttonHeight)
                            .style("opacity", 0);
-        
-        rect.on("mouseover", () => button.style("fill", "#333"))
-        rect.on("mouseout", () => button.style("fill", "#777"))
+
+        rect.on("mouseover", () => button.style("fill", "#333"));
+        rect.on("mouseout", () => button.style("fill", "#777"));
         rect.on("click", function() {
             if (codeActive) {
                 codeCells.each(function () {
@@ -76,30 +76,30 @@ requirejs(["d3"], d3 => {
                 codeActive = !codeActive;
             }
             button.style("opacity", codeActive ? 1 : 1/4);
-        })
+        });
     });
-    
+
     d3.svg("https://ionicons.com/ionicons/svg/logo-markdown.svg").then(function(svg) {
         var button = buttonContainer
                              .append("g")
-                                 .attr("transform", "translate(" + [width/4, 0] + ")")
+                                 .attr("transform", "translate(" + [0, 0] + ")")
                                  .style("fill", "#777")
-        
+
         button.style("opacity", markdownActive ? 1 : 1/4);
         button.html(svg.querySelector("svg").outerHTML);
-        
+
         var buttonWidth = button.node().getBoundingClientRect().width,
             buttonHeight = button.node().getBoundingClientRect().height;
-        
+
         var rect = button.insert("rect")
                            .attr("x", width/2 - buttonWidth/2)
                            .attr("y", height/2 - buttonHeight/2)
                            .attr("width", buttonWidth)
                            .attr("height", buttonHeight)
                            .style("opacity", 0);
-        
-        rect.on("mouseover", () => button.style("fill", "#333"))
-        rect.on("mouseout", () => button.style("fill", "#777"))
+
+        rect.on("mouseover", () => button.style("fill", "#333"));
+        rect.on("mouseout", () => button.style("fill", "#777"));
         rect.on("click", function() {
             if (markdownActive) {
                 markdownCells.each(function () {
@@ -114,6 +114,33 @@ requirejs(["d3"], d3 => {
                 markdownActive = !markdownActive;
             }
             button.style("opacity", markdownActive ? 1 : 1/4);
-        })
+        });
+    });
+
+    d3.svg("https://ionicons.com/ionicons/svg/md-play.svg").then(function(svg) {
+        var button = buttonContainer.append("g");
+
+        button.style("fill", "#777");
+        button.style("opacity", markdownActive ? 1 : 1/4);
+        button.html(svg.querySelector("svg").outerHTML);
+
+        var buttonWidth = button.node().getBoundingClientRect().width,
+            buttonHeight = button.node().getBoundingClientRect().height;
+
+        button.attr("transform", "translate(" + [width/3, 0] + ")")
+
+        var rect = button.insert("rect")
+                           .attr("x", width/2 - buttonWidth/2)
+                           .attr("y", height/2 - buttonHeight/2)
+                           .attr("width", buttonWidth)
+                           .attr("height", buttonHeight)
+                           .style("opacity", 0);
+
+        rect.on("mouseover", () => button.style("fill", "#333"));
+        rect.on("mouseout", () => button.style("fill", "#777"));
+        rect.on("click", function() {
+          Jupyter.notebook.kernel.interrupt();
+          Jupyter.notebook.execute_all_cells();
+        });
     });
 });
