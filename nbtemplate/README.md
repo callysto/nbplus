bedccc3dbce554fc88597702dbfad461b9a31c45
Eric Easthope <eric.easthope@me.com>
2018-07-23 16:10:49 -0700

    Add nbtemplate submodule and dependencies

---
+++ nbtemplate/README.md
@@ -0,0 +1,15 @@
+# nbtemplate
+An extension for loading custom templates into Jupyter notebooks.
+
+In any code cell within a Jupyter notebook (Python 3),
+
+```
+!pip install --upgrade --force-reinstall --user git+git://github.com/callysto/nbplus.git#egg=nbplus
+!jupyter nbextension enable --py --user nbtemplate
+```
+
+To disable `nbtemplate`,
+
+```
+!jupyter nbextension disable --py --user nbtemplate
+```
---
+++ nbtemplate/nbtemplate/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+'''
+
+def _jupyter_server_extension_paths():
+    return [{
+        'module':'nbtemplate'
+    }];
+
+def _jupyter_nbextension_paths():
+    return [
+        dict(
+            section='notebook',
+            src='static',             # path is relative to `nbtemplate` directory
+            dest='nbtemplate',        # directory in `nbextension/` namespace
+            require='nbtemplate/main' # _also_ in `nbextension/` namespace
+        ),
+        dict(
+            section='notebook',
+            src='static',                 # path is relative to `nbtemplate` directory
+            dest='nbtemplate',            # directory in `nbextension/` namespace
+            require='nbtemplate/selector' # _also_ in `nbextension/` namespace
+        )
+    ];
+
+def load_jupyter_server_extension(nbapp):
+    nbapp.log.info('Loaded nbtemplate!');
