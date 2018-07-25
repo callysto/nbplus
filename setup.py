a28396093ddb57dbdd22339352d2184e8fba12b8
Eric Easthope <eric.easthope@me.com>
2018-07-24 20:16:27 -0700

    Fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nbplus',
-    version='0.1dev',
+    version='0.0.dev0',
     packages=find_packages(),
     include_package_data=True
 )
