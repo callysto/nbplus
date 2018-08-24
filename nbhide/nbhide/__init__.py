8923fdfa7feb122af757654e02a96411a29feff8
Eric Easthope <eric.easthope@me.com>
2018-08-23 17:40:20 -0700

    Add appendToggles.js to extension paths

--- nbhide/nbhide/__init__.py
+++ nbhide/nbhide/__init__.py
@@ -4,12 +4,20 @@ def _jupyter_server_extension_paths():
     }];
 
 def _jupyter_nbextension_paths():
-    return [dict(
-        section="notebook",
-        src="static", # path is relative to `nbhide` directory
-        dest="nbhide", # directory in `nbextension/` namespace
-        require="nbhide/appendButtons" # _also_ in `nbextension/` namespace
-    )];
+    return [
+        dict(
+            section='notebook',
+            src='static',                  # path is relative to `nbhide` directory
+            dest='nbhide',                 # directory in `nbextension/` namespace
+            require='nbhide/appendButtons' # _also_ in `nbextension/` namespace
+        ),
+        dict(
+            section='notebook',
+            src='static',                      # path is relative to `nbhide` directory
+            dest='nbhide',                     # directory in `nbextension/` namespace
+            require='nbtemplate/appendToggles' # _also_ in `nbextension/` namespace
+        )
+    ];
 
 def load_jupyter_server_extension(nbapp):
     nbapp.log.info("nbhide enabled!");
