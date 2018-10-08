5a0c9afade351f61b9ae96a7e8496ec65e6efecf
Eric Easthope <eric.easthope@me.com>
2018-10-07 22:13:45 -0700

    Add overview toggle to template selector

--- nbtemplate/nbtemplate/static/templateSelector.js
+++ nbtemplate/nbtemplate/static/templateSelector.js
@@ -9,7 +9,7 @@ requirejs.config({ "paths": { "d3": "//d3js.org/d3.v5.min" } });
 requirejs(["d3"], d3 => {
 
     d3.json("https://raw.githubusercontent.com/callysto/notebook-templates/master/templateConfig.json").then(function(config) {
-      
+
       // specify arrangement and source URLs for notebook templates
       var templates = config.templates;
 
@@ -66,10 +66,58 @@ requirejs(["d3"], d3 => {
           .text("Add Template");
 
       // show template selector via dialog popover when icon clicked
-      templateIcon.on("click", showSelector);
+      templateIcon.on("click", (d3.event != null && d3.event.altKey) ? showOveriew : showSelector);
 
       // initialize selected template and template index variables
       var selectedTemplate, selectedIndex;
+
+      function showOveriew() {
+        d3.select("div.cell").style("width", "66%");
+        d3.select("#notebook-container")
+          .style("width", "100%")
+          .style("padding", "15px");
+        
+        var cellElement = this.element.parents(".cell"),
+            cellIndex = Jupyter.notebook.get_cell_elements().index(cellElement),
+            cellDiv = d3.select(Jupyter.notebook.get_cell(cellIndex).element[0]);
+
+        var notebookContainer = d3.select("#notebook-container"),
+            siteContainer = document.getElementById("site"),
+            leftMargin = d3.select('.input_prompt');
+
+        notebookContainer.selectAll('.movers').remove();
+
+        var notebookContainerWidth = notebookContainer.node().getBoundingClientRect().width,
+            leftMarginWidth = leftMargin.node().getBoundingClientRect().width,
+            cellWidth = cellDiv.node().getBoundingClientRect().width,
+            padding = parseInt(notebookContainer.style("padding"), 10);
+
+        var width = notebookContainerWidth - 2*padding,
+            height = siteContainer.getBoundingClientRect().height;
+
+        var marginAndPadding = leftMarginWidth + padding,
+            rightMargin = width - cellWidth + padding,
+            viewWidth = rightMargin - marginAndPadding,
+            viewTop = siteContainer.scrollTop + height/2 - viewWidth/2,
+            viewLeft = cellWidth + padding + rightMargin/2 - viewWidth/2;
+
+        var mover = d3.select("#notebook-container")
+            .append('div')
+            .attr('id', 'mover')
+            .attr('class', 'movers')
+            .style('border', '1px solid #cfcfcf')
+            .style('position', 'absolute')
+            .style('width', viewWidth.toString() + 'px')
+            .style('height', viewWidth.toString() + 'px')
+            .style('top', viewTop.toString() + 'px')
+            .style('left', viewLeft.toString() + 'px')
+
+        siteContainer.onscroll = () => {
+            mover.transition()
+                 .duration(500)
+                 .style('top', (siteContainer.scrollTop + height/2 - viewWidth/2).toString() + 'px')
+        }
+      }
 
       function showSelector() {
 
