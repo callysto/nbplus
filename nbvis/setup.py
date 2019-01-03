c436596c0262fe779002174efd381b771d3840f7
Eric Easthope <eric.easthope@me.com>
2019-01-03 15:18:06 -0800

    Update version

--- nbvis/setup.py
+++ nbvis/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def main():
     setup(
         name='nbvis',
-        version='0.0.dev0',
+        version='0.0.1',
         author='Eric Easthope',
         packages=find_packages(),
         include_package_data=True,
