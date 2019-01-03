71172460801a48ae09f2fac69fbf3fcdd9880a96
Eric Easthope <eric.easthope@me.com>
2019-01-03 15:26:59 -0800

    Remove temporary resources, housekeeping

--- nbvis/nbvis/magics.py
+++ nbvis/nbvis/magics.py
@@ -3,7 +3,8 @@
 
 """
 Jupyter notebook magics to queue and execute D3.js and MathBox.js code
-Assembled by Eric Easthope
+Assembled by Eric Easthope for Callysto
+MIT License
 """
 
 from .classes import Vis
@@ -27,27 +28,28 @@ class nbvisMagics(Magics):
     )
     def d3(self, line, cell):
         args = magic_arguments.parse_argstring(self.d3, line)
-        
-        # initialize d3_code container if it does not exist
+
         if 'd3_code' not in ip.user_ns.keys():
+            """Initialize d3_code container if it does not exist"""
             ip.user_ns['d3_code'] = []
             print('Initialized d3_code container!')
             
-        # reset d3_code container if it exists
         elif args.reset and 'd3_code' in ip.user_ns.keys():
+            """Reset d3_code container if it exists"""
             ip.user_ns['d3_code'] = []
             print('Reinitialized d3_code container!')
-            
-        # execute code cell immediately
-        if not args.queue: Vis(js=cell, _now=True)
-            
-        # queue code cell for later execution
+
+        if not args.queue:
+            """Execute code cell immediately"""
+            Vis(js=cell, _now=True)
+
         elif args.queue and cell not in ip.user_ns['d3_code']:
+            """Queue code cell for later execution"""
             ip.user_ns['d3_code'].append(cell)
             print('Code added to D3 visualization queue ...')
-        
-        # reject duplicate code cells
+
         elif cell in ip.user_ns['d3_code']:
+            """Reject duplicate code cells"""
             print('Duplicate code not added to D3 visualization queue ...')
 
     @cell_magic
@@ -62,27 +64,28 @@ class nbvisMagics(Magics):
     )
     def mathbox(self, line, cell):
         args = magic_arguments.parse_argstring(self.mathbox, line)
-        
-        # initialize mathbox_code container if it does not exist
+
         if 'mathbox_code' not in ip.user_ns.keys():
+            """Initialize mathbox_code container if it does not exist"""
             ip.user_ns['mathbox_code'] = []
             print('Initialized mathbox_code container!')
-            
-        # reset mathbox_code container if it exists
+
         elif args.reset and 'mathbox_code' in ip.user_ns.keys():
+            """Reset mathbox_code container if it exists"""
             ip.user_ns['mathbox_code'] = []
             print('Reinitialized mathbox_code container!')
-            
-        # execute code cell immediately
-        if not args.queue: Vis(js=cell, _now=True)
-            
-        # queue code cell for later execution
+
+        if not args.queue:
+            """Execute code cell immediately"""
+            Vis(js=cell, _now=True)
+
         elif args.queue and cell not in ip.user_ns['mathbox_code']:
+            """Queue code cell for later execution"""
             ip.user_ns['mathbox_code'].append(cell)
             print('Code added to MathBox visualization queue ...')
-        
-        # reject duplicate code cells
+
         elif cell in ip.user_ns['mathbox_code']:
+            """Reject duplicate code cells"""
             print('Duplicate code not added to MathBox visualization queue ...')
 
 ip = get_ipython()
