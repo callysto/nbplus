31a866378a76a28524683924e167f8135766da16
Eric Easthope <eric.easthope@me.com>
2019-03-22 04:11:58 -0700

    Remove D3 dependency

--- nbvis/nbvis/js/d3.js
+++ nbvis/nbvis/js/d3.js
@@ -1,14 +1,9 @@
 /* ...
 */
 
-requirejs.config({
-    paths: {
-      d3: "//d3js.org/d3.v5.min"
-    }
-});
-
-define("d3_view", ["@jupyter-widgets/base", "d3"], (widgets, d3) => {
+define("d3_view", ["@jupyter-widgets/base"], (widgets) => {
     var render = function() {
+        // var d3 = Object.assign({}, d3);
         D3View.__super__.render.apply(this, arguments);
 
         // listen for changes to the widget "value"
@@ -24,11 +19,11 @@ define("d3_view", ["@jupyter-widgets/base", "d3"], (widgets, d3) => {
 
         // set the container height, set container id
         div.style.height = model.get("height").toString() + "px";
-        d3.select(div).attr("id", model.get("name"));
+        div.id = model.get("name");
 
         // !important: these global variables must go away someday
         window[model.get("name") + "Model"] = model;
-        window[model.get("name") + "D3"] = d3;
+        // window[model.get("name") + "D3"] = d3;
 
         // !important: prompt the container to resize
         window.dispatchEvent(new Event("resize"));
