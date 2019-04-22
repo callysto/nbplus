e3b40790309bb23cf7530ac535fffed46b970774
Eric Easthope <eric.easthope@me.com>
2019-04-21 17:08:05 -0700

    Rename twoColumn magic to sidebyside

--- nblayout/nblayout/shape.py
+++ nblayout/nblayout/shape.py
@@ -20,23 +20,9 @@ module_directory = os.path.dirname(os.path.abspath(__file__))
 @magics_class
 class ShapeCell(Magics):
     @cell_magic
-    def columns(self, line):
-        js = '''
-            var cellIndex = Jupyter.notebook.get_cell_elements().index(this.element.parents('.cell')),
-                cellElement = Jupyter.notebook.get_cell(cellIndex).element[0],
-                cellInput = cellElement.getElementsByClassName('input')[0],
-                innerCell = cellElement.getElementsByClassName('inner_cell')[0],
-                outputWrapper = cellElement.getElementsByClassName('output_wrapper')[0];
-
-            cellElement.style.flexDirection = "row";
-            cellInput.style.width = "50%";
-            cellInput.style.height = "100%";
-            innerCell.style.height = "100%";
-            outputWrapper.style.width = "50%";
-            '''
-        display(Javascript(js))
-
-# toggle_filepath = os.path.join(module_directory, "createCellToggle.js")
-# with open(toggle_filepath, 'r') as createCellToggle:
-# 	display(Javascript(createCellToggle.read()))
+    def sidebyside(self, line, cell):
+        sbs_filepath = os.path.join(module_directory, 'sideBySide.js')
+        with open(sbs_filepath, 'r') as sbs:
+            display(Javascript(sbs.read()))
+        get_ipython().run_cell(cell)
 get_ipython().register_magics(ShapeCell);
