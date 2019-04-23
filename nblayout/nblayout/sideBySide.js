31d6b89ce232e3a973839a5d95d8d566d4993602
Eric Easthope <eric.easthope@me.com>
2019-04-23 06:04:30 -0700

    Add side-by-side with HTML nbconvert

--- nblayout/nblayout/sideBySide.js
+++ nblayout/nblayout/sideBySide.js
@@ -1,20 +1,41 @@
-/*
+/* ...
 */
 
-var cellIndex, cellElement;
-if (typeof Jupyter !== "undefined") {
-    var cellIndex = Jupyter.notebook.get_cell_elements().index(this.element.parents('.cell')),
-        cellElement = Jupyter.notebook.get_cell(cellIndex).element[0];
-}
-else {
-    var cellElement = element.closest(".cell")[0];
-}
+requirejs.config({
+    paths: {
+        "d3-require": "//cdn.jsdelivr.net/npm/d3-require@1?"
+    }
+});
 
-var cellInput = cellElement.getElementsByClassName('input')[0],
-    innerCell = cellElement.getElementsByClassName('inner_cell')[0],
-    outputWrapper = cellElement.getElementsByClassName('output_wrapper')[0];
+if (typeof Jupyter !== "undefined") $(document).ready(sideBySide)
+else sideBySide();
 
-cellElement.style.flexDirection = "row";
-cellInput.style.width = "50%";
-cellInput.style.height = "100%";
-outputWrapper.style.width = "50%";
+function sideBySide() {
+    var cellElement = element.parents(".cell")[0];
+
+    requirejs(["d3-require"], d3 => {
+        d3.require("d3-selection").then(d3 => {
+
+            if (typeof Jupyter === "undefined") {
+                d3.select(cellElement).selectAll("span").remove();
+                var cellText = d3.select(cellElement).select("pre").text();
+                cellText = cellText.substring(1);
+                d3.select(cellElement)
+                  .select("pre")
+                  .text(cellText);
+            }
+
+            d3.select(cellElement)
+              .style("flex-direction", "row");
+
+            d3.select(cellElement)
+              .select(".input")
+              .style("width", "50%")
+              .style("height", "100%");
+
+            d3.select(cellElement)
+              .select(".output_wrapper")
+              .style("width", "50%");
+        });
+    });
+}
