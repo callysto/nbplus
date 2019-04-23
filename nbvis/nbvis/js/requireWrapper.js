e1b0515d8670e1239dd0a2c5755c3279206d8283
Eric Easthope <eric.easthope@me.com>
2019-04-23 08:36:58 -0700

    Add check for waitForView

--- nbvis/nbvis/js/requireWrapper.js
+++ nbvis/nbvis/js/requireWrapper.js
@@ -4,20 +4,22 @@ requirejs.config({paths: #paths});
 
 var submodules = #submodules;
 
-const waitForView = (viewId) => {
-    return new Promise((resolve, reject) => {
-        var start = null;
-        window.requestAnimationFrame(step);
-        function step(timestamp) {
-            if (!start) start = timestamp;
-            var progress = timestamp - start,
-                el = document.getElementById(viewId);
+if (typeof window.waitForView === 'undefined') {
+    window.waitForView = (viewId) => {
+        return new Promise((resolve, reject) => {
+            var start = null;
+            window.requestAnimationFrame(step);
+            function step(timestamp) {
+                if (!start) start = timestamp;
+                var progress = timestamp - start,
+                    el = document.getElementById(viewId);
 
-            if (progress < 3000 && !el) { window.requestAnimationFrame(step); }
-            else { resolve(el); }
-        }
-  });
-};
+                if (progress < 3000 && !el) { window.requestAnimationFrame(step); }
+                else { resolve(el); }
+            }
+        });
+    };
+}
 
 requirejs(#modules, #moduleNames => {
     Promise.all([
