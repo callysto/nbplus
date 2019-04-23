47bc69b78c35bf46418fb34de45a02f83d9c66e4
Eric Easthope <eric.easthope@me.com>
2019-04-23 05:37:11 -0700

    Add cell hiding with HTML nbconvert

--- nblayout/nblayout/createCellToggle.js
+++ nblayout/nblayout/createCellToggle.js
@@ -1,88 +1,97 @@
 /* ...
 */
 
-requirejs.config({paths: {"d3": "//d3js.org/d3.v5.min"}});
-requirejs(["d3"], d3 => {
-
-    // select menubar container
-    var menuContainer = d3.select("div.container-fluid");
-    var kernelIndicator = d3.select("#kernel_indicator");
-
-    // get height of kernel indicator
-    var indicatorHeight = kernelIndicator
-                           .node()
-                               .getBoundingClientRect()
-                               .height;
-
-    var cellsHidden = true;
-
-    var svg = d3.select(element[0]).append("svg")
-            .attr("id", "toggle-svg")
-            .attr("width", indicatorHeight)
-            .attr("height", indicatorHeight)
-                .style("float", "right")
-                .style("margin-left", "5px")
-                .style("margin-right", "5px")
-            .classed("navbar-text", true);
-
-    var circle = svg.append("circle")
-           .attr("cx", indicatorHeight/2)
-           .attr("cy", indicatorHeight/2)
-           .attr("r", indicatorHeight)
-           .attr("fill", "#cfcfcf");
-
-    function addIcon(graphic) {
-        var iconNode = document.importNode(graphic.querySelector("svg"), true);
-        var icon = d3.select(iconNode)
-                .attr("id", "toggle-hiding")
-                .attr("fill", "#777");
-        svg.node().appendChild(iconNode);
-
-        // insert the icon node before the modal indicator
-        menuContainer.selectAll("#toggle-svg").remove();
-        menuContainer.node()
-                     .insertBefore(svg.node(), d3.select("#modal_indicator").node());
-
-        return icon;
+requirejs.config({
+    paths: {
+        "d3-require": "//cdn.jsdelivr.net/npm/d3-require@1?"
     }
-
-    window.clearSelection = () => {
-        if (window.getSelection) window.getSelection().removeAllRanges()
-        else if (document.selection) document.selection.empty();
-    }
-
-    window.toggleCellVisibility = (cell, visible) => {
-        var cellElement = cell.element[0],
-            innerCell = cellElement.getElementsByClassName("inner_cell")[0],
-            inputArea = cellElement.getElementsByClassName("input_area")[0];
-        inputArea.style.display = visible ? "unset" : "none";
-        innerCell.style.backgroundColor = visible ? "unset" : "#cfcfcf";
-        innerCell.style.opacity = visible ? "unset" : 1/10;
-    }
-
-    Promise.all([
-        d3.svg("https://ionicons.com/ionicons/svg/md-eye-off.svg"),
-        d3.svg("https://ionicons.com/ionicons/svg/md-eye.svg")
-    ]).then(icons => {
-        circle.style("opacity", 0);
-
-        var icon = addIcon(icons[0]);
-
-        svg.on("mouseover", () => icon.style("fill", "#333"))
-           .on("mouseout", () => icon.style("fill", "#777"));
-
-        svg.on("click", function() {
-            svg.select("#toggle-hiding").remove();
-            icon = addIcon(icons[cellsHidden ? 1 : 0]);
-            Jupyter.notebook.get_cells().forEach((cell, idx) => {
-                if (cell._metadata.hidden) {
-                    window.toggleCellVisibility(cell, cellsHidden ? true : false);
-                }
-            });
-            cellsHidden = !cellsHidden;
-        });
-
-        svg.append("title")
-           .text("Hide/Show cells");
-    });
 });
+
+if (typeof Jupyter !== "undefined") {
+    requirejs(["d3-require"], d3 => {
+        d3.require("d3-selection", "d3-fetch").then(d3 => {
+
+            // select menubar container
+            var menuContainer = d3.select("div.container-fluid"),
+                kernelIndicator = d3.select("#kernel_indicator");
+
+            // get height of kernel indicator
+            var indicatorHeight = kernelIndicator
+                                   .node()
+                                       .getBoundingClientRect()
+                                       .height;
+
+            var cellsHidden = true;
+
+            var svg = d3.select(element[0]).append("svg")
+                    .attr("id", "toggle-svg")
+                    .attr("width", indicatorHeight)
+                    .attr("height", indicatorHeight)
+                        .style("float", "right")
+                        .style("margin-left", "5px")
+                        .style("margin-right", "5px")
+                    .classed("navbar-text", true);
+
+            var circle = svg.append("circle")
+                   .attr("cx", indicatorHeight/2)
+                   .attr("cy", indicatorHeight/2)
+                   .attr("r", indicatorHeight)
+                   .attr("fill", "#cfcfcf");
+
+            function addIcon(graphic) {
+                var iconNode = document.importNode(graphic.querySelector("svg"), true);
+                var icon = d3.select(iconNode)
+                        .attr("id", "toggle-hiding")
+                        .attr("fill", "#777");
+                svg.node().appendChild(iconNode);
+
+                // insert the icon node before the modal indicator
+                menuContainer.selectAll("#toggle-svg").remove();
+                menuContainer.node()
+                             .insertBefore(svg.node(), d3.select("#modal_indicator").node());
+
+                return icon;
+            }
+
+            Promise.all([
+                d3.svg("https://ionicons.com/ionicons/svg/md-eye-off.svg"),
+                d3.svg("https://ionicons.com/ionicons/svg/md-eye.svg")
+            ]).then(icons => {
+                circle.style("opacity", 0);
+
+                var icon = addIcon(icons[0]);
+
+                svg.on("mouseover", () => icon.style("fill", "#333"))
+                   .on("mouseout", () => icon.style("fill", "#777"));
+
+                svg.on("click", function() {
+                    svg.select("#toggle-hiding").remove();
+                    icon = addIcon(icons[cellsHidden ? 1 : 0]);
+                    Jupyter.notebook.get_cells().forEach((cell, idx) => {
+                        if (cell._metadata.hide_cell) {
+                            window.toggleCellVisibility(cell.element[0], cellsHidden ? true : false);
+                        }
+                    });
+                    cellsHidden = !cellsHidden;
+                });
+
+                svg.append("title")
+                   .text("Hide/Show cells");
+            });
+        });
+    });
+}
+
+window.clearSelection = () => {
+    if (window.getSelection) window.getSelection().removeAllRanges()
+    else if (document.selection) document.selection.empty();
+}
+
+window.toggleCellVisibility = (cellElement, visible) => {
+    var innerCell = cellElement.getElementsByClassName("inner_cell")[0],
+        inputArea = cellElement.getElementsByClassName("input_area")[0];
+
+    inputArea.style.display = visible ? "unset" : "none";
+    innerCell.style.backgroundColor = visible ? "unset" : "#cfcfcf";
+    innerCell.style.opacity = visible ? "unset" : 1/10;
+}
--- nblayout/nblayout/hideCell.js
+++ nblayout/nblayout/hideCell.js
@@ -1,24 +1,39 @@
 /* ...
 */
 
-requirejs.config({paths: {"d3": "//d3js.org/d3.v5.min"}});
-requirejs(["d3"], d3 => {
-    var cellElement = this.element.parents(".cell")[0],
-        cellIndex = Jupyter.notebook.get_cell_elements().index(cellElement),
-        cell = Jupyter.notebook.get_cell(cellIndex);
+$(document).ready(function() {
+    var cellElement = element.parents(".cell")[0];
 
-    cell._metadata.hidden = true;
-    
-    d3.select(cellElement)
-      .select(".input_area")
-      .style("display", "none");
-    
-    d3.select(cellElement)
-      .select(".inner_cell")
-      .style("background-color", "#cfcfcf")
-      .style("opacity", 1/10)
-      .on("dblclick", function() {
-          window.toggleCellVisibility(cell, true);
-          window.clearSelection();
-      });
+    if (typeof Jupyter !== "undefined") {
+        var cellIndex = Jupyter.notebook.get_cell_elements().index(cellElement),
+            cell = Jupyter.notebook.get_cell(cellIndex);
+        cell._metadata.hide_cell = true;
+    }
+
+    requirejs(["d3-require"], d3 => {
+        d3.require("d3-selection").then(d3 => {
+
+            if (typeof Jupyter === "undefined") {
+                d3.select(cellElement).selectAll("span").remove();
+                var cellText = d3.select(cellElement).select("pre").text();
+                cellText = cellText.substring(1);
+                d3.select(cellElement)
+                  .select("pre")
+                  .text(cellText);
+            }
+
+            d3.select(cellElement)
+              .select(".input_area")
+              .style("display", "none");
+
+            d3.select(cellElement)
+              .select(".inner_cell")
+              .style("background-color", "#cfcfcf")
+              .style("opacity", 1/10)
+              .on("dblclick", function() {
+                  window.toggleCellVisibility(cellElement, true);
+                  window.clearSelection();
+              });
+        });
+    });
 });
